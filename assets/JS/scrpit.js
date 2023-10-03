1484ab13c84f60f8d73ff000027d6c63c799c695
Adrian <adrianribeiroprofissional@gmail.com>
2023-10-02 21:56:09 -0300

    enviando para o github

--- assets/JS/scrpit.js
+++ assets/JS/scrpit.js
@@ -1,11 +1,12 @@
 function  clicou(){
     const ul = document.querySelector('ul');
 
-    if(ul.style.display == 'none'){
-        ul.style.display = 'flex';
-    } else {
+    if(ul.style.display == 'flex'){
         ul.style.display = 'none';
+    } else {
+        ul.style.display = 'flex';
     }
+
 }
 const menu = document.querySelector('.menu');
 menu.addEventListener('click', clicou);
